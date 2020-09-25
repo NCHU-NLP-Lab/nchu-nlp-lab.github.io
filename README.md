6fcafbc067266d5ac8712031cf5ef8b62b2cab01
Tomy Hsieh <tomy0000000@gmail.com>
2020-09-26 05:25:09 +0800

    Organizing
    * Update README
    * Add LICENSE
    * Add .gitignore
    * Update Dependencies
    * Update webpack & babel configs

---
+++ README.md
@@ -0,0 +1,32 @@
+# UDIC Website
+
+實驗室的網站
+
+## Structure
+```bash
+├── Dockerfile
+├── README.md
+├── package-lock.json
+├── package.json # node dependencies & commands
+├── requirements.txt # flask dependencies
+├── server.js # run node server
+├── server.py # run flask server
+├── src # source code
+└── webpack.config.js 
+```
+
+## Install
+
+```bash
+npm install
+```
+## Development  
+
+```bash
+npm run dev
+```
+## Compile for production
+
+```bash
+npm run build
+```
--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,35 @@
-const path = require('path');
+const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    output: {
-        path: path.resolve(__dirname, './output'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test   : /\.(js|jsx)$/,
-                loaders: ['babel-loader'],
-                include: path.join(__dirname, 'src')
-            }, {
-                test: /\.(scss|sass|css)$/i,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },{
-                test: /.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
-                loader: 'url-loader',
-                options: {
-                    limit: 50000,
-                }
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/index.html",
-            filename: "./index.html"
-        })
-    ]
-}
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loaders: ["babel-loader"],
+        include: path.join(__dirname, "src"),
+      },
+      {
+        test: /\.(scss|sass|css)$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+        loader: "url-loader",
+        options: {
+          limit: 50000,
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
