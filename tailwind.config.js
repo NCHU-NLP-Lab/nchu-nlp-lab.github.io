a53cdc6380adf460e67c4a33e77f3db2012750c2
Tomy Hsieh <tomy0000000@gmail.com>
2020-10-07 03:53:46 +0800

    Reduce CSS & Structure Rebuild

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  future: {},
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          300: "#6692c8",
+          400: "#4278b8",
+          700: "#08192d",
+        },
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
--- webpack.config.js
+++ webpack.config.js
@@ -4,70 +4,96 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-module.exports = {
-  output: {
-    path: path.resolve(__dirname, "dist"),
-  },
-  entry: {
-    index: [
-      "@fortawesome/fontawesome-free/js/all.js",
-      "./src/assets/css/index.css",
+module.exports = (env, argv) => {
+  return {
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "[name].js",
+    },
+    entry: {
+      index: [
+        "@fortawesome/fontawesome-free/js/all.js",
+        "./src/assets/css/index.css",
+      ],
+      querator: [
+        "@fortawesome/fontawesome-free/js/all.js",
+        "./src/assets/css/querator.css",
+      ],
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "index.html",
+        chunks: ["index"],
+      }),
+      new HtmlWebPackPlugin({
+        template: "./src/querator.html",
+        filename: "querator.html",
+        chunks: ["querator"],
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].bundle.css",
+        chunkFilename: "[id].css",
+      }),
+      new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery",
+      }),
+      new CleanWebpackPlugin(),
     ],
-    querator: [
-      "@fortawesome/fontawesome-free/js/all.js",
-      "./src/assets/css/querator.css",
-    ],
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "index.html",
-      chunks: ["index"],
-    }),
-    new HtmlWebPackPlugin({
-      template: "./src/querator.html",
-      filename: "querator.html",
-      chunks: ["querator"],
-    }),
-    new MiniCssExtractPlugin(),
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
-    }),
-    new CleanWebpackPlugin(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(html)$/,
-        use: ["html-loader"],
-      },
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
-        test: /\.(png|jpe?g|svg|gif)$/i,
-        use: [
-          {
-            loader: "url-loader",
+    module: {
+      rules: [
+        {
+          test: /\.(html)$/,
+          use: ["html-loader"],
+        },
+        {
+          test: /\.css$/i,
+          use: [
+            "style-loader",
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                hmr: argv.mode === "development",
+              },
+            },
+            {
+              loader: "css-loader",
+              options: {
+                importLoaders: 1,
+              },
+            },
+            "postcss-loader",
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|svg|gif)$/i,
+          use: [
+            {
+              loader: "url-loader",
+              options: {
+                name: "img/[contenthash].[ext]",
+                limit: 8192,
+              },
+            },
+          ],
+        },
+        {
+          test: /\.m?js$/i,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
             options: {
-              name: "img/[contenthash].[ext]",
-              limit: 8192,
+              presets: ["@babel/preset-env"],
             },
           },
-        ],
-      },
-      {
-        test: /\.m?js$/i,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
         },
-      },
-    ],
-  },
+      ],
+    },
+    devServer: {
+      watchContentBase: true,
+      contentBase: path.resolve(__dirname, "dist"),
+      open: true,
+    },
+  };
 };
