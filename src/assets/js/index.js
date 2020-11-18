aa14270dbd8bb833c5c2341c7dafc24dd9e083d6
Tomy Hsieh <tomy0000000@gmail.com>
2020-11-19 03:15:57 +0800

    Minor Updates
    * Add Verdict Network demo links
    * Add recruit info
    * Add member GitHub links
    * Temporarily remove Navbar
    * Adjust map size

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -3,16 +3,16 @@ import AOS from "aos";
 // require("../lottie/lf20_ynle8i40.json");
 // require("../lottie/lf20_yYOpBn.json");
 
-let check_nav_active = () => {
-  $(".nav-item").removeClass("active");
-  $($(".nav-item").slice(1).get().reverse()).each((index, element) => {
-    const section_element = $($(element).children("a").attr("href"));
-    if (window.pageYOffset >= section_element.offset().top) {
-      $(element).addClass("active");
-      return false;
-    }
-  });
-};
+// let check_nav_active = () => {
+//   $(".nav-item").removeClass("active");
+//   $($(".nav-item").slice(1).get().reverse()).each((index, element) => {
+//     const section_element = $($(element).children("a").attr("href"));
+//     if (window.pageYOffset >= section_element.offset().top) {
+//       $(element).addClass("active");
+//       return false;
+//     }
+//   });
+// };
 
 let setting_member_animation_delay = () => {
   $(".member-container").each((container_index, container) => {
@@ -38,26 +38,26 @@ let setting_member_animation_delay = () => {
 $(() => {
   $(window).on("resize", setting_member_animation_delay);
 
-  $(".nav-item").on("click", (event) => {
-    $(".nav-item").removeClass("active");
-    $(event.target).addClass("active");
-  });
+  // $(".nav-item").on("click", (event) => {
+  //   $(".nav-item").removeClass("active");
+  //   $(event.target).addClass("active");
+  // });
 });
 
-const banner_height = $("#banner").outerHeight();
+// const banner_height = $("#banner").outerHeight();
 $(window).on("scroll", () => {
-  // Navbar Color
-  if (window.pageYOffset >= banner_height) {
-    $("#navbar")
-      .addClass("bg-white text-blue-700")
-      .removeClass("bg-transparent text-white");
-  } else {
-    $("#navbar")
-      .addClass("bg-transparent text-white")
-      .removeClass("bg-white text-blue-700");
-  }
-
-  check_nav_active();
+  // // Navbar Color
+  // if (window.pageYOffset >= banner_height) {
+  //   $("#navbar")
+  //     .addClass("bg-white text-blue-700")
+  //     .removeClass("bg-transparent text-white");
+  // } else {
+  //   $("#navbar")
+  //     .addClass("bg-transparent text-white")
+  //     .removeClass("bg-white text-blue-700");
+  // }
+  //
+  // check_nav_active();
   setting_member_animation_delay();
   AOS.init();
 });
